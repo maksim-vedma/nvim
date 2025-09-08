6f26436002044d959699ad8ec2cdd4fead239e38
Maxime Montagne <maxime@drosalys.fr>
2025-09-08 16:46:30 +0200

    replaced bufferline with mini.tabline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -3,13 +3,13 @@ vim.pack.add({
     -- icons
     { src = "https://github.com/nvim-mini/mini.icons" },
     -- bufferline
-    { src = "https://github.com/akinsho/bufferline.nvim"},
-    { src = "https://github.com/j-hui/fidget.nvim"},
+    { src = "https://github.com/nvim-mini/mini.tabline" },
+    { src = "https://github.com/j-hui/fidget.nvim" },
     -- toasts
     -- fidget (lsp loading)
     -- noice (notify ?)
 })
 
 require("mini.icons").setup()
-require("bufferline").setup({})
+require("mini.tabline").setup()
 require("fidget").setup({})

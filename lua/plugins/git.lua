771a3a54415125309e044e31d09b2b000428e67e
Maxime Montagne <maxime@drosalys.fr>
2025-09-14 17:24:47 +0200

    updated git related keymaps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -9,7 +9,11 @@ require('gitsigns').setup({
     signcolumn = true,
     current_line_blame = true,
 })
-vim.keymap.set("n", "<leader>gb", '<cmd>Gitsigns blame<CR>')
 
-vim.keymap.set("n", "<leader>gs", '<cmd>Git<CR>')
-vim.keymap.set("n", "<leader>gp", '<cmd>Git push<CR>')
+vim.keymap.set("n", "<leader>gb", ':Gitsigns blame<CR>', { silent = true })
+vim.keymap.set("n", "<leader>gs", ':Git<CR>', { silent = true })
+vim.keymap.set("n", "<leader>gd", ':Gdiffsplit<CR>', { silent = true })
+
+-- vim.keymap.set("n", "<leader>ga", ':Git add %<CR>', { silent = true })
+-- vim.keymap.set("n", "<leader>gp", ':Git push<CR>', { silent = true })
+-- vim.keymap.set("n", "<leader>gP", ':Git pull<CR>', { silent = true })

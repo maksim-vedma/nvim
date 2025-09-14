c8cf2a841a230b269d0d50cee59f07bb1be740ef
Maxime Montagne <maxime@drosalys.fr>
2025-09-14 17:25:10 +0200

    added Pick buffers

--- lua/plugins/pick.lua
+++ lua/plugins/pick.lua
@@ -8,5 +8,6 @@ local opt = { silent = true }
 vim.keymap.set('n', '<leader>f', ":Pick files<CR>", opt) -- Duplicate is intentionnal - defaults to Pick git files
 vim.keymap.set('n', '<leader>ff', ":Pick files<CR>", opt)
 vim.keymap.set('n', '<leader>fF', ":Pick files tool='git'<CR>", opt)
+vim.keymap.set('n', '<leader>fd', ":Pick buffers<CR>", opt) -- "fd" simply because it's faster to write than "fb"
 vim.keymap.set('n', '<leader>fg', ":Pick grep<CR>", opt)
 vim.keymap.set('n', '<leader>fh', ":Pick help<CR>", opt)

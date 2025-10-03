87623e1778be8d0376ef85b23fe6f75caa8588ea
Maksim Montagne <maxime@drosalys.fr>
2025-10-03 14:54:56 +0200

    fix php lsp completion trigger

--- lua/plugins/auto-tag.lua
+++ lua/plugins/auto-tag.lua
@@ -2,4 +2,10 @@ vim.pack.add({
     { src = "https://github.com/windwp/nvim-ts-autotag" },
 })
 
-require("nvim-ts-autotag").setup({})
+require("nvim-ts-autotag").setup({
+    per_filetype = {
+        ["php"] = {
+            enable_close = false -- needed to enable lsp suggestion on "->"
+        }
+    }
+})

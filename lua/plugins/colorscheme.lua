a4c75a22aa4f5261b1e1ba9bc6ee00b081c3aeb8
Maksim Montagne <maxime@drosalys.fr>
2025-11-08 08:14:06 +0100

    Colorscheme: transparent bg + changed virtual text color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,16 @@
 vim.pack.add({
     { src = "https://github.com/webhooked/kanso.nvim" },
-    {src = "https://github.com/oahlen/iceberg.nvim" },
-    -- { src = "https://github.com/mcauley-penney/techbase.nvim" },
+    { src = "https://github.com/oahlen/iceberg.nvim" },
+    { src = "https://github.com/mcauley-penney/techbase.nvim" },
 })
 
-require('kanso').setup({})
+require('kanso').setup({
+    transparent = false,
+    overrides = function(colors)
+        return {
+            CodeiumSuggestion = { fg = colors.palette.gray3 },
+        }
+    end,
+})
 
-vim.cmd("colorscheme kanso")
+vim.cmd("colorscheme kanso-zen")
